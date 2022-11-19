03c9b5269d87c39346ede0cce3de162315811995
dlcastrob <laura.castrobrns@uanl.edu.mx>
2022-11-19 00:44:50 -0600

    full row

--- AVANCE2/Ventanas/AGREGAR_PRODUCTO.Designer.cs
+++ AVANCE2/Ventanas/AGREGAR_PRODUCTO.Designer.cs
@@ -214,8 +214,10 @@ namespace AVANCE2
             this.Column9});
             this.dataGridView1.Location = new System.Drawing.Point(725, 54);
             this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
             this.dataGridView1.RowHeadersWidth = 51;
             this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
             this.dataGridView1.Size = new System.Drawing.Size(919, 399);
             this.dataGridView1.TabIndex = 11;
             // 
